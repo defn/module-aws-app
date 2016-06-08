91046157027e702d3fb99d6742c839c08040bda6
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-08 14:18:59 -0700

    dont set up remote state yet, chicken/egg

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region = "${var.provider_region}"
 }
-
-resource "terraform_remote_state" "env" {
-  backend = "s3"
-  config {
-    bucket = "${var.bucket_remote_state}"
-    key = "${var.bucket_remote_state}/env-${var.context_org}-${var.context_env}.tfstate"
-    region = "${var.provider_region}"
-  }
-}
