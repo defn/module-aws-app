0776000f7da2457f8d50eda6a3d03a3f69726425
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-29 00:20:54 -0700

    make a security group for the app

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ provider "aws" {
 }
 
 variable "app_name" {}
+
+resource "aws_security_group" "sg" {
+  name = "${var.app_name}"
+  description = "Application ${var.app_name}"
+
+  vpc_id = "${data.terraform_remote_state.env.vpc_id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
