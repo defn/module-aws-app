1991c492316f625bf7d573383cf8ff1f7308d6dc
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-29 00:04:36 -0700

    link to env-xxx remote state

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
 provider "aws" {
 }
 
-data "terraform_remote_state" "env" {
-  backend = "s3"
-  config {
-    bucket = "${var.bucket_remote_state}"
-    key = "${var.bucket_remote_state}/env-${var.context_org}-${var.context_env}.tfstate"
-  }
-}
-
 variable "app_name" {}
