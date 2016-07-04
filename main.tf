ffed7dd055cf632162f78af0986bb7642773a0d4
Tom Bombadil <amanibhavam@destructuring.org>
2016-07-03 22:43:52 -0700

    dont namespace vpc stuff, use Name tag

--- main.tf
+++ main.tf
@@ -4,12 +4,16 @@ provider "aws" {
 variable "app_name" {}
 
 resource "aws_security_group" "sg" {
-  name = "${var.context_org}-${var.context_env}-${var.app_name}"
+  name = "${var.app_name}"
   description = "Application ${var.app_name}"
 
   vpc_id = "${data.terraform_remote_state.env.vpc_id}"
 
   lifecycle {
     create_before_destroy = true
+  }
+
+  tags {
+    "Name" = "${var.context_org}-${var.context_env}-${var.app_name}"
   }
 }
